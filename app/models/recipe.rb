b339394d6bea1c7151c054036f64bf7abfd3b42c
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-02-07 04:26:43 -0500

    added optional: true to recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,14 +1,14 @@
 class Recipe < ApplicationRecord
 
-  belongs_to :user
+  belongs_to :user, optional: true
 
-  belongs_to :category
+  belongs_to :category, optional: true
   accepts_nested_attributes_for :category, reject_if: :all_blank
 
   has_many :comments
 
   has_many :instructions, dependent: :destroy
-  accepts_nested_attributes_for :instructions, reject_if: :all_blank, allow_destroy: :true 
+  accepts_nested_attributes_for :instructions, reject_if: :all_blank, allow_destroy: true 
 
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
