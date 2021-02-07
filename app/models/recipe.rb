be938bb043eaa8d80700cba7e2a978784ceae666
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-02-07 09:43:41 -0500

    updated recipe validations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ApplicationRecord
 
-  belongs_to :user, optional: true
+  belongs_to :user
 
   belongs_to :category, optional: true
   accepts_nested_attributes_for :category, reject_if: :all_blank
@@ -8,14 +8,17 @@ class Recipe < ApplicationRecord
   has_many :instructions, dependent: :destroy
   accepts_nested_attributes_for :instructions, reject_if: :all_blank, allow_destroy: true 
 
-  has_many :recipe_ingredients
+  has_many :recipe_ingredients, dependent: :destroy
   has_many :ingredients, through: :recipe_ingredients
-  
+  accepts_nested_attributes_for :recipe_ingredients
+
   validates :title, presence: true
+  validates :instructions, presence: true
+  validates :description, presence: true
 
 
-  def self.alphabetize
-    order(title: :asc)
-  end
+  scope :alphabetize, -> { order("title asc")}
+
+  
 
 end
