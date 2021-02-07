1a3098ed5696f93280c4ea3f4232b99aca68b8e6
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-02-06 21:26:46 -0500

    added helper methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,28 @@
 class ApplicationController < ActionController::Base
+
+  helper_method :user_signed_in?, :current_user
+
+  def user_signed_in?
+    !!session[:user_id]
+  end
+
+  def redirect_if_not_logged_in
+    redirect_to '/login' if !user_signed_in?
+  end
+
+  def redirect_if_logged_in
+    redirect_to user_path(current_user) if user_signed_in?
+  end
+
+  def current_user
+    User.find_by_id(session[:user_id]) if user_signed_in?
+  end
+
+  def find_by_recipe_id(recipe)
+    Recipe.find_by(id: params[:recipe_id])
+  end
+
+  def find_recipe
+    @recipe = Recipe.find_by_id(params[:id])
+  end
 end
