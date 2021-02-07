1dccce525f2ff3f97e29b4ada4f8a8b4cca42c0f
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-02-07 00:11:08 -0500

    added user relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  has_secure_password
+  
+  has_many :recipes
+  has_many :comments
+  
+  validates :name, presence: true
+
+  validates :email, presence: true, uniqueness: true 
 end
