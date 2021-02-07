49047ba60a602cb2a0edf9599a835d697e45b559
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-02-06 21:42:04 -0500

    added routes & logic to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,36 @@
 class UsersController < ApplicationController
+
+  def index
+    @user = User.all
+   
+  end
+  
+  
+    def new
+      @user = User.new
+    end
+  
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        session[:user_id] = @user.id
+        redirect_to recipes_path 
+      else
+        flash.now[:notice] = "Email provided already has an account"
+        render :new
+      end
+    end
+  
+    def show
+      @user = User.find_by_id(params[:id])
+      if @category
+        render :show
+      end
+    end
+  
+    private
+      def user_params
+        params.require(:user).permit(:name, :email, :password)
+      end
+  
 end
