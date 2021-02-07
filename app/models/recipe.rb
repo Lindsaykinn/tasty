fe6e89f272cda56064e0909446008e75d0359dae
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-02-06 23:49:10 -0500

    added relationships to recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,23 @@
 class Recipe < ApplicationRecord
+
+  belongs_to :user
+
+  belongs_to :category
+  accepts_nested_attributes_for :category, reject_if: :all_blank
+
+  has_many :comments
+
+  has_many :instructions, dependent: :destroy
+  accepts_nested_attributes_for :instructions, reject_if: :all_blank, allow_destroy: :true 
+
+  has_many :recipe_ingredients
+  has_many :ingredients, through: :recipe_ingredients
+  
+  validates :title, presence: true
+
+
+  def self.alphabetize
+    order(title: :asc)
+  end
+
 end
