60359bf9a00b0163155c69998e7ee58fbcf8e165
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-02-06 20:49:25 -0500

    created recipes resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_07_014646) do
+ActiveRecord::Schema.define(version: 2021_02_07_014801) do
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "title"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
