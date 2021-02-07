3ea128317ea3b98ad8afff6ebab63298ef33d047
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-02-06 23:48:49 -0500

    added routes & logic to recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,25 @@
 class RecipesController < ApplicationController
+  #need before_action find_recipe here
+
+  def index
+    @recipes = Recipe.all
+    if @category
+      @recipes = @category.recipes 
+    else
+      @recipes = Recipe.all.alphabetize
+    end
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = current_user.recipes.build(recipe_params)
+    if @recipe.save
+      redirect_to recipe_path(@recipe)
+    else
+      render :new
+    end
+  end
 end
