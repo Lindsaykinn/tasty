7916e9a14badcbafe7b2b692634e7b7497416eca
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-02-07 16:47:37 -0500

    fixed find_by_id methods in recipes cont

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,36 +2,50 @@ class RecipesController < ApplicationController
   before_action :redirect_if_not_logged_in
   before_action :find_recipe, only: [:show, :edit, :update, :destroy]
   before_action :find_category, only: [:index, :new, :create]
+  before_action :redirect_if_not_owner, only: [:edit, :update, :destroy]
 
   def index
     @recipes = Recipe.all
     if @category
       @recipes = @category.recipes 
     else
-      @recipes = Recipe.all
+      @recipes = Recipe.all.sorted
     end
   end
 
   def new
     @recipe = Recipe.new
-    @recipe.recipe_ingredients.build
+    @recipe.instructions.build
   end
 
   def create
-    params[:recipe][:user_id] = current_user.id
     @recipe = Recipe.new(recipe_params)
+    @recipe.category.user = current_user
+    # params[:recipe][:user_id] = current_user.id
       if @recipe.save
-        redirect_to recipe_path(@recipe)
+        #i don't understand why i need this extra code when params are passed into the build above
+        if @category
+          redirect_to category_recipes_path(@category) 
+        else
+          redirect_to recipes_path
+        end
       else
         flash.now[:error] = @recipe.errors.full_messages
-        render :new        
+        if @category 
+        render :new_category_recipe
+        else
+        render :new
+        end
       end
-  end
+    end
 
   def show
-    if @category
-      redirect_to category_recipe_path(@category)
-    end
+    @category = Category.find_by_id(params[:id])
+    @category = @recipe.category
+  end
+
+  def user_recipes
+    @recipes = current_user.recipes 
   end
 
   def edit
@@ -63,24 +77,27 @@ class RecipesController < ApplicationController
     if params[:category_id]
       @category = Category.find_by_id(params[:id])
     end
-  end
+  
+  def redirect_if_not_owner
+    if current_user.id != @recipe.user_id
+      flash[:notice] = "You cannot delete or update a recipe if you are not the owner."
+      return redirect_to recipe_path unless @recipe 
+      redirect_to recipe_path unless current_user.id == @recipe.user_id
+      end
+    end
+  end  
+
+
 
   def recipe_params
     params.require(:recipe).permit(
     :title, 
-    :description, 
-    :user_id,
+    :description,
     :category_id,
+    category_attributes: [:name],
     instructions_attributes: [
-      :id, 
-      :step, 
-      :_destroy],
-    recipe_ingredients_attributes: [
-      :measurement,
-      ingredient_attributes: [
-        :ingredient_name
-      ]
-    ]
+      :step
+      ]    
   )
   end
 
