ea86c50f02b368e86f780f60b9ef00ddb048b036
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-02-06 22:00:26 -0500

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,35 @@
 Rails.application.routes.draw do
-  resources :recipe_ingredients
+  
+  root to: 'static#home'
+
   resources :ingredients
+
   resources :comments
-  resources :categories
-  resources :recipes
+
+  resources :categories do
+    resources :recipes, only: [:index, :show]
+  end
+
+  resources :recipes do
+    resources :comments, only: [:index, :new, :create]
+    resources :recipe_ingredients, only: [:index, :new, :create]
+    resources :categories, only: [:index, :new, :create]
+  end  
+
+  resources :recipe_ingredients do
+    collection do
+      delete 'destroy_multiple'
+    end
+  end
+    
   resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/signup', to: 'users#new', as: 'signup'
+  post '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new', as: 'login'
+  post '/login', to: 'sessions#create'
+
+  delete '/logout', to: 'sessions#destroy', as: 'destroy_user_session'
+
+  match '/auth/:google_oauth2/callback' => 'sessions#omniauth', via: [:get, :post]
+
 end
